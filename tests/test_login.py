7cf8ee904f53d68174cc0c831976e35ad3378861
Federico Peche <fedepeche27@gmail.com>
2025-10-20 13:59:14 -0300

    test: Se crea el código base para 'TC-001' Login, con screenshots antes y despues del login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,21 @@
-##(TC-003)##
+# TC-001: Login con credenciales válidas
+from utils.saucedemo_pages import LoginPage, InventoryPage
+import os
+
+def test_login_success(driver):
+    login = LoginPage(driver)
+    login.open()
+
+    # screenshot antes de login (evidencia)
+    driver.save_screenshot(os.path.join("reports", "screenshots", "TC-001_before_login.png"))
+
+    login.login("standard_user", "secret_sauce")
+
+    inventory = InventoryPage(driver)
+    inventory.wait_for_page()
+
+    # screenshot luego de login (evidencia)
+    driver.save_screenshot(os.path.join("reports", "screenshots", "TC-001_after_login.png"))
+
+    assert "/inventory.html" in driver.current_url, "No se redirigió a /inventory.html"
+    assert "Products" in inventory.get_title_text() or "Swag Labs" in driver.title
